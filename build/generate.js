d7284a60480352f084187b8366daa2251bb6cd37
Tarun Mangukiya <tarunmangukiya@hotmail.com>
2021-05-13 11:55:57 +0530

    feat: Unicons thinline

--- build/generate.js
+++ build/generate.js
@@ -1,30 +1,33 @@
-const path = require('path')
-const fs = require('fs-plus')
-const cheerio = require('cheerio')
-const upperCamelCase = require('uppercamelcase')
+const path = require("path");
+const fs = require("fs-plus");
+const cheerio = require("cheerio");
+const upperCamelCase = require("uppercamelcase");
 
-const iconsComponentPath = path.join(process.cwd(), 'icons')
-const iconsIndexPath = path.join(process.cwd(), 'index.js')
-const uniconsConfig = require('@iconscout/unicons/json/solid.json')
+const iconsComponentPath = path.join(process.cwd(), "icons");
+const iconsIndexPath = path.join(process.cwd(), "index.js");
+const uniconsConfig = require("@iconscout/unicons/json/thinline.json");
 
 // Clear Directories
-fs.removeSync(iconsComponentPath)
-fs.mkdirSync(iconsComponentPath)
+fs.removeSync(iconsComponentPath);
+fs.mkdirSync(iconsComponentPath);
 
-const indexJs = []
+const indexJs = [];
 
-uniconsConfig.forEach(icon => {
-  const baseName = `uis-${icon.name}`
-  const location = path.join(iconsComponentPath, `${baseName}.js`)
-  const name = upperCamelCase(baseName)
-  const svgFile = fs.readFileSync(path.resolve('node_modules/@iconscout/unicons', icon.svg), 'utf-8')
+uniconsConfig.forEach((icon) => {
+  const baseName = `uit-${icon.name}`;
+  const location = path.join(iconsComponentPath, `${baseName}.js`);
+  const name = upperCamelCase(baseName);
+  const svgFile = fs.readFileSync(
+    path.resolve("node_modules/@iconscout/unicons", icon.svg),
+    "utf-8"
+  );
 
-  let data = svgFile.replace(/<svg[^>]+>/gi, '').replace(/<\/svg>/gi, '')
+  let data = svgFile.replace(/<svg[^>]+>/gi, "").replace(/<\/svg>/gi, "");
   // Get Path Content from SVG
   const $ = cheerio.load(data, {
-    xmlMode: true
-  })
-  const svgPath = $('path').attr('d')
+    xmlMode: true,
+  });
+  const svgPath = $("path").attr("d");
 
   const template = `import React from 'react';
 import PropTypes from 'prop-types';
@@ -53,14 +56,14 @@ ${name}.defaultProps = {
   size: '24',
 };
 
-export default ${name};`
+export default ${name};`;
 
-  fs.writeFileSync(location, template, 'utf-8')
+  fs.writeFileSync(location, template, "utf-8");
 
   // Add it to index.js
-  indexJs.push(`export { default as ${name} } from './icons/${baseName}'`)
-})
+  indexJs.push(`export { default as ${name} } from './icons/${baseName}'`);
+});
 
-fs.writeFileSync(iconsIndexPath, indexJs.join('\n'), 'utf-8')
+fs.writeFileSync(iconsIndexPath, indexJs.join("\n"), "utf-8");
 
-console.log(`Generated ${uniconsConfig.length} icon components.`)
+console.log(`Generated ${uniconsConfig.length} icon components.`);
